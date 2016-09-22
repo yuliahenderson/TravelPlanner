c25c610d3aa6961bd5d9bd631fed9d81005989a0
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-22 11:47:47 -0400

    add route for the homepage content to render

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,11 +1,13 @@
 import React, { Componetn } from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import Main from '../components/main.jsx';
+import Homepage from '../components/homepage.jsx';
 
 const Routes = () => {
   return (
     <Router history={hashHistory}>
       <Route path="/" component={Main}>
+        <IndexRoute component={Homepage} />
       </Route>
     </Router>
   );
