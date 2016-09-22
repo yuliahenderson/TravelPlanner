d4cf4c62178c22f1c80ca273179de8e2dd4987d0
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-22 14:30:11 -0400

    add ability for user to input username and password to login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import firebase from '../../firebase.config.js';
 
 class Login extends Component {
   constructor() {
@@ -7,6 +9,27 @@ class Login extends Component {
       username: '',
       password: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    const stateObj = {};
+    const stateKey = e.target.name;
+    stateObj[stateKey] = e.target.value;
+    this.setState(stateObj);
+  }
+  handleSubmit() {
+    const { username, password } = this.state;
+    firebase.auth()
+      .signInWithEmailAndPassword(username, password)
+      .catch((err) => {
+        const errorCode = err.code;
+        const errorMessage = err.message;
+        console.log(err)
+      })
+      .then(() => {
+        this.props.router.push('/userboard');
+      })
   }
   render() {
     return (
@@ -14,16 +37,16 @@ class Login extends Component {
         <h2>Keep your trips organized and share with friends</h2>
         <div id="login-form">
           <div>
-            <input name="username" type="text" placeholder="username" />
+            <input name="username" onChange={this.handleChange} type="text" placeholder="username" />
           </div>
           <div>
-            <input name="password" type="password" placeholder="password" />
+            <input name="password" onChange={this.handleChange} type="password" placeholder="password" />
           </div>
-          <button className="button">Login</button>
+          <button className="button" onClick={this.handleSubmit}>Login</button>
         </div>
       </div>
     );
   }
 }
 
-export default Login;
+export default withRouter (Login);
