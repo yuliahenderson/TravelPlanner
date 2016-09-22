9c062a31350e4bb43c18a720504be5e7f15dd067
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-22 11:47:03 -0400

    add headline for the homepage content

---
+++ src/components/homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h2>Keep your trips organized and share with friends</h2>
+    </div>
+  );
+}
+
+export default Homepage;
