c6ab1ec1d2cb0b5b8c48e25bd20f06ef01102db3
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-22 14:31:05 -0400

    add ability for user to input username and password to register

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import firebase from '../../firebase.config.js';
 
 
 class Register extends Component {
@@ -8,6 +10,30 @@ class Register extends Component {
       username: '',
       password: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    const stateObj = {};
+    const stateKey = e.target.name;
+    stateObj[stateKey] = e.target.value;
+    this.setState(stateObj);
+  }
+  handleSubmit() {
+    const { username, password } = this.state;
+    firebase.auth()
+     .createUserWithEmailAndPassword(username, password)
+     .catch((err) => {
+       console.log(err);
+     })
+     .then((user) => {
+       firebase.database().ref('users')
+       .child(user.uid)
+       .set({ email: username });
+     })
+      .then(() => {
+        this.props.router.push('/userboard');
+      });
   }
   render() {
     return (
@@ -15,16 +41,16 @@ class Register extends Component {
         <h2>Keep your trips organized and share with friends</h2>
         <div id="register-form">
           <div>
-            <input name="username" type="text" placeholder="username" />
+            <input name="username" onChange={this.handleChange} type="text" placeholder="username" />
           </div>
           <div>
-            <input name="password" type="password" placeholder="password" />
+            <input name="password" onChange={this.handleChange} type="password" placeholder="password" />
           </div>
-          <button className="button">Register</button>
+          <button className="button" onClick={this.handleSubmit}>Register</button>
         </div>
       </div>
     );
   }
 }
 
-export default Register;
+export default withRouter (Register);
