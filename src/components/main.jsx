c139afa42bc9eb35550abe30c41c26a68bf6bd7b
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-22 15:28:53 -0400

    add links to login/register and sign out with user authentication

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import firebase from '../../firebase.config.js';
 
 const propTypes = {
   children: React.PropTypes.element.isRequired,
@@ -7,12 +9,51 @@ const propTypes = {
 class Main extends Component {
   constructor(){
     super();
+    this.state = {
+      loggedIn: false,
+    };
+    this.signOut = this.signOut.bind(this);
+  }
+  componentWillMount() {
+    setTimeout(() => {
+      firebase.auth().onAuthStateChanged((user) => {
+        this.setState({
+          loggedIn: (user !== null),
+        });
+      });
+    }, 200);
+  }
+  signOut() {
+    firebase.auth()
+     .signOut()
+     .then(() => {
+       console.log('user signed out');
+     });
+  }
+  loggedInLinks() {
+    if (!this.state.loggedIn) {
+      return (
+        <div>
+          <Link to="/login" id="login">Login / </Link>
+          <Link to="/register" id="register">Register</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div id="sign-out">
+          <Link to="/" onClick={this.signOut}>Sign Out</Link>
+        </div>
+      );
+    }
   }
   render() {
     return (
       <div>
         <div id="nav">
           <h1>TRAVEL PLANNER</h1>
+          {
+            this.loggedInLinks()
+          }
         </div>
         <div id="content">
           {this.props.children}
