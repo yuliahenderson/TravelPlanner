f9adedb8b30a554193acdb34206e4665e3a6c45d
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-22 13:33:46 -0400

    route to register and login paths

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -2,12 +2,16 @@ import React, { Componetn } from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import Main from '../components/main.jsx';
 import Homepage from '../components/homepage.jsx';
+import Register from '../components/register.jsx';
+import Login from '../components/login.jsx';
 
 const Routes = () => {
   return (
     <Router history={hashHistory}>
       <Route path="/" component={Main}>
         <IndexRoute component={Homepage} />
+        <Route path="register" component={Register} />
+        <Route path="login" component={Login} />
       </Route>
     </Router>
   );
