1db56f0da312d2ea87e22124b2c840cf23a05a7c
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-22 15:29:38 -0400

    add path to userboard

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,9 +1,11 @@
-import React, { Componetn } from 'react';
+import React, { Component } from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import Main from '../components/main.jsx';
 import Homepage from '../components/homepage.jsx';
 import Register from '../components/register.jsx';
 import Login from '../components/login.jsx';
+import Userboard from '../components/userboard.jsx';
+import requireAuth from '../auth.js';
 
 const Routes = () => {
   return (
@@ -12,10 +14,11 @@ const Routes = () => {
         <IndexRoute component={Homepage} />
         <Route path="register" component={Register} />
         <Route path="login" component={Login} />
+        <Route path="userboard" component={Userboard} onEnter={requireAuth} />
       </Route>
     </Router>
   );
-}
+};
 
 export default Routes;
 
