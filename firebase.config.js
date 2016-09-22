bed680426b63e55898a134514d4d7fa1d914c2bb
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-22 14:31:56 -0400

    add firebase data to store users emails

---
+++ firebase.config.js
@@ -0,0 +1,12 @@
+  const firebase = require('firebase');
+
+  var config = {
+    apiKey: "AIzaSyBlZUq7XVSyEnJbvWMKH9bhjNqPNg0BnNA",
+    authDomain: "travel-planner-65a26.firebaseapp.com",
+    databaseURL: "https://travel-planner-65a26.firebaseio.com",
+    storageBucket: "travel-planner-65a26.appspot.com",
+    messagingSenderId: "1048192343473"
+  };
+  firebase.initializeApp(config);
+
+module.exports = firebase;
