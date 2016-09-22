1868e4ead1fe4f1e73169f6362db688ee298e937
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-22 11:22:35 -0400

    add route for the Routes file to render in DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('Ready!')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './config/routes.jsx';
+
+ReactDOM.render(< Routes />, document.getElementById('root'));
