d127a717d8f66b4ada3e4400bb5c0f91ff0c9e07
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-23 15:19:24 -0400

    add ability to submit, save and edit new post

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
 const propTypes = {
-  date: React.PropTypes.string,
+  dateTo: React.PropTypes.string,
+  dateBack: React.PropTypes.string,
   destination: React.PropTypes.string,
-  daysOfStay: React.PropTypes.number,
+  handlePublish: React.PropTypes.func,
+  handleDelete: React.PropTypes.func,
   id: React.PropTypes.string,
 };
 
@@ -11,35 +13,98 @@ class Post extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      localDate: this.props.date || '',
+      localDateTo: this.props.dateTo || '',
+      localDateBack: this.props.dateBack || '',
       localDestination: this.props.destination || '',
-      localDaysOfStay: this.props.daysOfStay || '',
     };
+    this.handleEditOfDateTo = this.handleEditOfDateTo.bind(this);
+    this.handleEditOfDateBack = this.handleEditOfDateBack.bind(this);
+    this.handleEditOfDestination = this.handleEditOfDestination.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.isSaved = this.isSaved.bind(this);
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      localDateTo: nextProps.dateTo || '',
+      localDateBack: nextProps.dateBack || '',
+      localDestination: nextProps.destination || '',
+    });
+  }
+  handleEditOfDateTo(e) {
+    const newDateTo = e.target.value;
+    this.setState({
+      localDateTo: newDateTo,
+    });
+  }
+  handleEditOfDateBack(e) {
+    const newDateBack = e.target.value;
+    this.setState({
+      localDateBack: newDateBack,
+    });
+  }
+  handleEditOfDestination(e) {
+    const newDestination = e.target.value;
+    this.setState({
+      localDestination: newDestination,
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handlePublish({
+      id: this.props.id,
+      dateTo: this.state.localDateTo,
+      dateBack: this.state.localDateBack,
+      destination: this.state.localDestination,
+    });
+    this.setState({ saved: true });
+  }
+  handleDeleteClick() {
+    this.props.handleDelete(this.props.id);
+  }
+  isSaved() {
+    return this.props.dateTo === this.state.localDateTo &&
+           this.props.destination === this.state.localDestination &&
+           this.props.dateBack === this.state.localDateBack;
   }
   render() {
+    let activeButtons;
+    if (this.isSaved()) {
+      activeButtons = (
+        <div className="active-buttons">
+          <button onClick={this.handleDeleteClick}>x</button>
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <form className="post-view">
+      <div className={this.isSaved() ? 'saved' : 'not-saved'}>
+        <form className="post-view" onSubmit={this.handleSubmit}>
           <input
-            type="text"
-            name="month"
-            value={this.state.localDate}
+            type="date"
+            name="dateTo"
+            value={this.state.localDateTo}
+            onChange={this.handleEditOfDateTo}
+          />
+          <input
+            type="date"
+            name="dateBack"
+            value={this.state.localDateBack}
+            onChange={this.handleEditOfDateBack}
           />
           <input
             type="text"
             name="destination"
             value={this.state.localDestination}
-          />
-          <input
-            type="number"
-            name="daysOfStay"
-            value={this.state.daysOfStay}
+            onChange={this.handleEditOfDestination}
           />
           <input
             type="submit"
             value="save"
+            className="hidden"
           />
         </form>
+        {activeButtons}
       </div>
     );
   }
