49154386c8c87c5b3d0617d050529f23433931eb
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-23 15:21:29 -0400

    add ability to save, edit, update posts in a database

--- src/components/userboard.jsx
+++ src/components/userboard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import request from 'superagent';
 import PostList from './postList.jsx';
+import Post from './post.jsx';
 
 
 class Userboard extends Component {
@@ -9,6 +10,10 @@ class Userboard extends Component {
     this.state = {
       posts: [],
     };
+    this.handlePublish = this.handlePublish.bind(this);
+    this.httpUpdatePost = this.httpUpdatePost.bind(this);
+    this.httpPublishPost = this.httpPublishPost.bind(this);
+    this.httpDeletePost = this.httpDeletePost.bind(this);
   }
   componentDidMount() {
     this.httpGetPosts();
@@ -24,20 +29,51 @@ class Userboard extends Component {
                  const individualPostData = postsData[id];
                  return {
                    id,
-                   date: individualPostData.date,
+                   dateTo: individualPostData.dateTo,
+                   dateBack: individualPostData.dateBack,
                    destination: individualPostData.destination,
-                   daysOfStay: individualPostData.daysOfStay,
                  };
                });
              }
              this.setState({ posts });
            });
   }
+  handlePublish({ id, dateTo, dateBack, destination }) {
+    if (id) {
+      this.httpUpdatePost({ id, dateTo, dateBack, destination });
+    } else {
+      this.httpPublishPost({ dateTo, dateBack, destination });
+    }
+  }
+  httpDeletePost(id) {
+    const url = `https://travel-planner-65a26.firebaseio.com/posts${id}.json`;
+    request.del(url)
+           .then(() => {
+             this.httpGetPosts();
+           });
+  }
+  httpUpdatePost({ id, dateTo, dateBack, destination }) {
+    const url = `https://travel-planner-65a26.firebaseio.com/posts${id}.json`;
+    request.patch(url)
+           .send({ dateTo, dateBack, destination })
+           .then(() => {
+             this.httpGetPosts();
+           });
+  }
+  httpPublishPost({ dateTo, dateBack, destination }) {
+    const url = 'https://travel-planner-65a26.firebaseio.com/posts.json';
+    request.post(url)
+           .send({ dateTo, dateBack, destination })
+           .then(() => {
+             this.httpGetPosts();
+           });
+  }
 
   render() {
     return (
       <div>
-        <PostList posts={this.state.posts} />
+        <PostList handleDelete={this.httpDeletePost} handlePublish={this.handlePublish} posts={this.state.posts} />
+        <Post handleDelete={this.httpDeletePost} handlePublish={this.handlePublish} />
       </div>
     );
   }
