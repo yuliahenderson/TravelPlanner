0c5c9cd339ba08d576165020f58563229ed9a8a3
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-23 09:47:17 -0400

    connect postsList with userboard

--- src/components/userboard.jsx
+++ src/components/userboard.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import request from 'superagent';
+import PostList from './postList.jsx';
 
-const Userboard = () => {
-  return(
-    <div>
-      <h2>Userboard</h2>
-    </div>
-  );
+
+class Userboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+  componentDidMount() {
+    this.httpGetPosts();
+  }
+  httpGetPosts() {
+    const url = 'https://travel-planner-65a26.firebaseio.com/posts.json';
+    request.get(url)
+           .then((response) => {
+             const postsData = response.body;
+             let posts = [];
+             if (postsData) {
+               posts = Object.keys(postsData).map((id) => {
+                 const individualPostData = postsData[id];
+                 return {
+                   id,
+                   date: individualPostData.date,
+                   destination: individualPostData.destination,
+                   daysOfStay: individualPostData.daysOfStay,
+                 };
+               });
+             }
+             this.setState({ posts });
+           });
+  }
+
+  render() {
+    return (
+      <div>
+        <PostList posts={this.state.posts} />
+      </div>
+    );
+  }
 }
+
 export default Userboard;
