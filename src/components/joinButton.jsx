017813eb6f9eb3430f45602207b47b6e9b8f255e
Julia Henderson <yulia.p.henderson@gmail.com>
2016-09-26 10:50:43 -0400

    add functionality for join button to render on the page

--- src/components/joinButton.jsx
+++ src/components/joinButton.jsx
@@ -1,1 +1,21 @@
-joinButton.jsx
+import React, { Component } from 'react';
+
+const propTypes = {
+  joinCount: React.PropTypes.number,
+  handleJoinClick: React.PropTypes.func,
+};
+
+class JoinButton extends Component {
+  render() {
+    return (
+      <div className="join-button">
+        <button onClick={this.props.handleJoinClick}>JOIN</button>
+        <p className="join-count">{this.props.joinCount}</p>
+      </div>
+    );
+  }
+}
+
+JoinButton.propTypes = propTypes;
+
+export default JoinButton;
