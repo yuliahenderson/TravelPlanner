00301a796b11d28366da33ca09bec6b707ec6758
Julia Henderson <yulia.p.henderson@gmail.com>
2016-10-16 21:28:39 -0400

    add new path for the images

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
     <div>
       <h2>Keep your trips organized and share with friends</h2>
       <div className="photo">
-        <img className="main-image" src='https://github.com/yuliahenderson/TravelPlanner/blob/gh-pages/images/travel.png' />
+        <img className="main-image" src='../static/images/travel.png' />
       </div>
     </div>
   );
